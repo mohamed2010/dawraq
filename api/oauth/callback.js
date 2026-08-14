d1eeab1293da0e5ddaa9e003464cd889c824f3b7
mohamed2010 <mr.mohamed87@gmail.com>
2026-08-14 13:02:25 +0000

    Use standard JavaScript Vercel functions

--- api/oauth/callback.js
+++ api/oauth/callback.js
@@ -1,5 +1,4 @@
 import "dotenv/config";
 import { app } from "../../server/_core/app.ts";
 
-// Explicit OAuth callback function used by the Manus login redirect URI.
 export default app;
--- api/trpc/[...trpc].js
+++ api/trpc/[...trpc].js
@@ -1,5 +1,4 @@
 import "dotenv/config";
 import { app } from "../../server/_core/app.ts";
 
-// Exact catch-all function for the relative tRPC client path: /api/trpc/*.
 export default app;
